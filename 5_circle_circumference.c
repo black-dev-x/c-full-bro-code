86310832c9d97722b35c0ee017e84321d349a796
black-dev-x <thiago.dev.x@gmail.com>
2024-06-16 19:06:23 -0300

    circumference and area

---
+++ 5_circle_circumference.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  const double PI = 3.14159;
+  double radius;
+  double circumference;
+  double area;
+
+  printf("\nPlease enter the radius of the circle: ");
+  scanf("%lf", &radius);
+
+  circumference = 2 * PI * radius;
+  area = PI * radius * radius;
+  
+  printf("The circumference of the circle is: %lf\n", circumference);
+  printf("The area of the circle is: %lf\n", area);
+}
