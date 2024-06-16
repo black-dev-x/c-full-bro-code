5b01b234c57570f985baffe9c3be204d374a8200
black-dev-x <thiago.dev.x@gmail.com>
2024-06-16 15:21:04 -0300

    update

---
+++ 1_beginning.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() 
+{
+  printf("I like pizza!\n");
+  printf("It's really good!\n");
+  // if there are no errors, return 0, else, return 1
+  int age = 21;
+  float gpa = 2.05;
+  
+  return 0;
+}
