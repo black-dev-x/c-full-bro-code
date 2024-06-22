87d5822eb43a5e777fcbcb94cb41d9dbfe787c0a
black-dev-x <thiago.dev.x@gmail.com>
2024-06-22 12:44:45 -0300

    order

--- 7_temperature.c
+++ 7_temperature.c
@@ -11,13 +11,13 @@ int main(){
   unit = toupper(unit);
 
   if (unit == 'C'){
-    printf("The temperature in Fahrenheit is: %.2f\n", (temp * 9/5) + 32);
     printf("Enter the temperature in Celsius: ");
     scanf("%f", &temp);
+    printf("The temperature in Fahrenheit is: %.2f\n", (temp * 9/5) + 32);
   } else if (unit == 'F'){
-    printf("The temperature in Celsius is: %.2f\n", (temp - 32) * 5/9);
     printf("Enter the temperature in Fahrenheit: ");
     scanf("%f", &temp);
+    printf("The temperature in Celsius is: %.2f\n", (temp - 32) * 5/9);
   } else {
     printf("%c is not a valid unit of measurement\n", unit);
   }
